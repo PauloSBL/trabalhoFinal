facf8474cf2edd1434bed043da0492dbda9eb667
PauloSBL <114448029+PauloSBL@users.noreply.github.com>
2023-02-07 11:34:01 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+const mysql = require('mysql2')
 const express = require('express');
 const { engine } = require('express-handlebars');
 
 const app = express();
 
+const conn = mysql.createConnection({
+    host:'localhost',
+    user:"root",
+    password:"",
+    database:"teste"
+})
+
+
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.use(express.static('public'))
-app.set('views', './views');
+app.set('views', './app/views');
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home')
 });
 
 app.get('/login', (req, res) => {
---
+++ public/js/login.js
@@ -0,0 +1,4 @@
+document.getElementById('btnLogin').addEventListener('click', function() {
+    document.getElementById('loginCard').classList.add('fade-out');
+    
+  });
